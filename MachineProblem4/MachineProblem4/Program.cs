b6dced820058ac93dbd2ba0b5915855e6c5e1ea4
Yen Sangalang <yengestrudelsangalang@iskolarngbayan.pup.edu.ph>
2021-05-09 00:46:59 +0800

    Add files via upload

---
+++ MachineProblem4/MachineProblem4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MachineProblem4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program allows you to input two integer numbers and counts the number between them that is divisible by 5\r\n");
+
+            int count = 0;
+
+            Console.Write("Enter the first number:  ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            for (int range = num1; range <= num2; range ++)
+            {
+                if (range % 5 == 0) count++;
+            }
+
+            Console.WriteLine("\r\nThere are {0} numbers that are divisible by 5 from {1} to {2}", count, num1, num2);
+        }
+    }
+}
---
+++ MachineProblem5/MachineProblem5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MachineProblem5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program lets you input two whole numbers and show the greater number between them\r\n");
+
+            Console.Write("Enter the first number:  ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            string GreatNum1 = (num1 >= num2) ? "higher" : "lower";
+            string GreatNum2 = (num1 <= num2) ? "higher" : "lower";
+
+            Console.WriteLine("\r\n{0} is {1}" + " while {2} is {3}", num1, GreatNum1, num2, GreatNum2);
+        }
+    }
+}
